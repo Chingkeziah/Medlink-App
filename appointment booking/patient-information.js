3e013f54ccb47d1c7a2c6c271c1d7883a4c0f043
Chingkeziah <chingkeziah@gmail.com>
2024-04-24 00:29:53 +0100

    feat: Added a patient information page

---
+++ appointment booking/patient-information.js
@@ -0,0 +1,11 @@
+// for the side bar
+window.onload = function () {
+    var sidebar = document.getElementById("sidebar");
+    var content = document.getElementById("wrapper");
+   
+    // Set sidebar height to match content height
+  sidebar.style.height = wrapper.offsetHeight + "px";
+  };
+
+//   pop up
+
